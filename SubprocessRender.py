75efa370a73a40c51182bcd986c7c7fc6bf3dfe5
Shelby Jueden <shelby@jueden.org>
2018-03-19 18:24:00 -0700

    thread usage implimented

--- SubprocessRender.py
+++ SubprocessRender.py
@@ -21,12 +21,17 @@ thread_options = [
     ("3", "Except", "Use this many less than all threads", 3),
 ]
 
+def uiupdate(self,context):
+    scene = context.scene
+    if scene.subprocess_render.thread_usage == "1":
+        scene.subprocess_render.thread_count = scene.render.threads
+
 
 class SubprocessRenderProperties(bpy.types.PropertyGroup):
     '''Subprocess Render  settings'''
-    thread_count = bpy.props.IntProperty(name="Thread Count", default=4)
+    thread_count = bpy.props.IntProperty(name="Thread Count", default=4,min=1)
     thread_usage = bpy.props.EnumProperty(
-        items=thread_options, name = "Thread Setting", default="1")
+        items=thread_options, name = "Thread Setting", default="1",update=uiupdate)
 
 
 class SubprocessRenderPanel(bpy.types.Panel):
@@ -46,6 +51,7 @@ class SubprocessRenderPanel(bpy.types.Panel):
         row.prop(scene.subprocess_render, "thread_usage", expand=True)
         col = layout.column()
         col.prop(scene.subprocess_render,"thread_count")
+        col = layout.column()
         col.operator(SubprocessRenderStart.bl_idname)
 
 
@@ -60,11 +66,19 @@ class SubprocessRenderStart(bpy.types.Operator):
         frame_ranges = []
         commands = []
         frame_count = scene.frame_end - scene.frame_start
-        command = str('blender -b "' + str(bpy.path.abspath(bpy.data.filepath)) + '" -s %d -e %d -a')
-        frames_per_thread = int(frame_count / scene.subprocess_render.thread_count)
 
+        threads =scene.render.threads
+        if scene.subprocess_render.thread_usage == "1":
+            threads = scene.render.threads
+        elif scene.subprocess_render.thread_usage == "2":
+            threads = scene.subprocess_render.thread_count
+        elif scene.subprocess_render.thread_usage == "3":
+            threads = threads - scene.subprocess_render.thread_count
+
+        command = str('blender -b "' + str(bpy.path.abspath(bpy.data.filepath)) + '" -s %d -e %d -a')
+        frames_per_thread = int(frame_count / threads)
         # Build commands for subprocesses
-        for i in range(0,scene.subprocess_render.thread_count):
+        for i in range(0,threads):
             frame_ranges.append([i*frames_per_thread+1,(i+1)*frames_per_thread])
             commands.append(command % (frame_ranges[-1][0],frame_ranges[-1][1]))
 
@@ -72,9 +86,11 @@ class SubprocessRenderStart(bpy.types.Operator):
         frame_ranges[0][0] = scene.frame_start
         frame_ranges[-1][1] = scene.frame_end
 
+        pprint(frame_ranges)
+
         # Begin threads
-        processes = [Popen(cmd, shell=True) for cmd in commands]
-        for p in processes: p.wait()
+        #processes = [Popen(cmd, shell=True) for cmd in commands]
+        #for p in processes: p.wait()
 
         # Render audio
         audio_path=os.path.dirname(bpy.path.abspath(scene.render.filepath))+"/audio.flac"
